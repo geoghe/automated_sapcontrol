1639b1d26bfdf8ae804cb09d5e4949b978159de7
Ariel Mendoza <ariel.azem@gmail.com>
2017-11-21 13:32:07 -0600

    validation of action and systems, including action notifications

--- automated_stop.sh
+++ automated_stop.sh
@@ -36,6 +36,7 @@ _INSTANCE2="$(tail -n1 /tmp/automation)"
 sudo su -l "$_SID2"adm -c "sapcontrol -nr "$_INSTANCE2" -function GetSystemInstanceList"
 exit
 XOF
+echo -e "$(tput setaf 1)STOP signal was sent for this system $(tput sgr0)"
 }
 
 start(){
@@ -51,34 +52,51 @@ _INSTANCE2="$(tail -n1 /tmp/automation)"
 sudo su -l "$_SID2"adm -c "sapcontrol -nr "$_INSTANCE2" -function GetSystemInstanceList"
 exit
 XOF
+echo -e "$(tput setaf 2)START signal was sent for this system $(tput sgr0)"
+}
+
+exit_msg(){
+        echo -e "$(tput setaf 1)cbb_project.sh: Missing arguments.\nPlease specify the action\nUsage:\n\tcbb_project.sh [status|start|stop]\n$(tput sgr0)"
+        exit 1
 }
 
 _function="${1:-"status"}"
 
-while read _SID
-do
-        if [[ -n "$_SID" ]]; then
-                echo "$_SID" | grep -o -E '[[:alnum:]]{3}' > /dev/null 2>&1
-                _SID_check="$?"
-                nc -vz vhene"$_SID"ci.rot.hec.enelint.global 22 > /dev/null 2>&1
-                _SSH_check="$?"
+echo "$_function" | grep -E 'start|status|stop' >/dev/null || exit_msg
 
-                if [[ "$_SID_check" -eq '0' && "$_SSH_check" -eq '0' ]]; then
-                        echo -e "$(tput setaf 1) $_SID $(tput sgr0)"
-                        case "$_function" in
-                                status) status ;;
-                                start)  start  ;;
-                                stop)   stop   ;;
-                        esac
+echo "As per the ENE_SID_LIST, you want to $(tput setaf 3)"$_function"$(tput sgr0) the following systems:\n"
+cat ENE_SID_LIST
+echo "Type complete [yes|no] if you want to proceed:\n"
+read _yesno
+
+if [[ "$_yesno" == 'yes' ]]; then
+        while read _SID
+        do
+                if [[ -n "$_SID" ]]; then
+                        echo "$_SID" | grep -o -E '[[:alnum:]]{3}' > /dev/null 2>&1
+                        _SID_check="$?"
+                        nc -vz vhene"$_SID"ci.rot.hec.enelint.global 22 > /dev/null 2>&1
+                        _SSH_check="$?"
+
+                        if [[ "$_SID_check" -eq '0' && "$_SSH_check" -eq '0' ]]; then
+                                echo -e "$(tput setaf 1) $_SID $(tput sgr0)"
+                                case "$_function" in
+                                        status) status ;;
+                                        start)  start  ;;
+                                        stop)   stop   ;;
+                                esac
+                        else
+                                echo -e "$(tput setaf 1)"
+                                [[ "$_SID_check" -eq '0' ]] || echo "$_SID doesnt meet the convention of an SID"
+                                [[ "$_SSH_check" -eq '0' ]] || echo "$_SID server is not reachable, check is a valid SID"
+                                echo -e "$(tput sgr0)"
+                        fi
                 else
                         echo -e "$(tput setaf 1)"
-                        [[ "$_SID_check" -eq '0' ]] || echo "$_SID doesnt meet the convention of an SID"
-                        [[ "$_SSH_check" -eq '0' ]] || echo "$_SID server is not reachable, check is a valid SID"
+                        echo "NULL - No SID declared"
                         echo -e "$(tput sgr0)"
                 fi
-        else
-                echo -e "$(tput setaf 1)"
-                echo "NULL - No SID declared"
-                echo -e "$(tput sgr0)"
-        fi
-done < ENE_SID_LIST
+        done < ENE_SID_LIST
+else
+        echo "$_yesno was typed, exit."
+fi
